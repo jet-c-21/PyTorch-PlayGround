d4fef2000438aa358d4627ea37a085ddd3731bbd
jet-c-21 <67193495+jet-c-21@users.noreply.github.com>
2022-01-22 13:56:34 +0800

    add first code

---
+++ .virtual_documents/0.Basic/Basic.ipynb.py
@@ -0,0 +1,108 @@
+import torch
+
+
+x = torch.empty(2, 3)  # 2 row, 3 column
+x
+
+
+y = torch.empty(3, 2, 3)
+y, y.size()
+
+
+rand_tensor = torch.rand(2, 2)
+rand_tensor
+
+
+zeros_tensor = torch.zeros(2, 2)
+zeros_tensor, zeros_tensor.size()
+
+
+ones_tensor = torch.ones(2, 2)
+print(ones_tensor.type())
+print(ones_tensor.dtype)
+
+
+x = torch.tensor([1, 2])
+y = torch.tensor([3, 4])
+z = y.add(x)
+print(f"y={y} z={z}")
+
+
+y.add_(x)
+print(f"y={y}")
+
+
+x = torch.rand(5,3)
+
+
+x
+
+
+x[:, 0] # to get only first column
+
+
+x[1, :] # row 1 with all columns
+
+
+x[3, 1] # row 3 col 1
+
+
+x[3, 1].item() # to get the actual value
+
+
+x = torch.rand(4,4)
+
+
+x
+
+
+y = x.view(16)
+
+
+y
+
+
+y = x.view(-1, 8)
+y
+
+
+y.size()
+
+
+import numpy as np
+
+
+a = torch.ones(5)
+a
+
+
+b = a.numpy()
+b, type(b)
+
+
+a.add_(1)
+a
+
+
+b
+
+
+a = np.ones(5)
+a
+
+
+b = torch.from_numpy(a)
+b
+
+
+
+
+
+a += 1
+a
+
+
+b
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # PyTorch-PlayGround
 The implemented code from [THIS TUTORIAL](https://www.youtube.com/playlist?list=PLqnslRFeH2UrcDBWF5mfPGpqQDSta6VK4) on YouTube
+
+## Environment
+- conda
+```
+conda create --name pytorch python=3.8 -y
+```
+- Jupyter Kernel
+```json
+pip install ipykernel
+python -m ipykernel install --user --name pytorch --display-name "Pytorch Playground"
+```
+- torch
+```
+conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch -y
+```
