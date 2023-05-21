61db00469c7e29f0a7a834fccd1c0647bc115a3d
Jet (Ta-Wei Chien) <67193495+jet-c-21@users.noreply.github.com>
2023-05-21 13:56:07 +0800

    fin 01

---
+++ Tutorials/mrdbourke/README.md
@@ -0,0 +1,3 @@
+# [YouTube Video](https://youtu.be/V_xro1bcAuA)
+# [GitHub Repo](https://github.com/mrdbourke/pytorch-deep-learning)
+# [Web Page](https://www.learnpytorch.io/)
---
+++ WALL/view_torch_src_code.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+"""
+Author: Jet C.
+GitHub: https://github.com/jet-c-21
+Create Date: 5/19/23
+"""
+import torch
+
+with torch.inference_mode():
+    pass
+
+torch.nn.L1Loss
+
